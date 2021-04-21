e5d99cfb3b6940ad3a2f24f4e08c82d99ceb8b6f
Qingxiong Ma <qma@ucmo.edu>
2021-04-21 08:57:56 -0500

    port number change

--- runserver.py
+++ runserver.py
@@ -4,7 +4,7 @@ from flaskr import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '8080'))
     except ValueError:
-        PORT = 5555
+        PORT = 8080
     app.run(HOST, PORT)
